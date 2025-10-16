14591e406def1b30a0f41ac882622a87200289c8
ABrickToTheHead <87443023+ABrickToTheHead@users.noreply.github.com>
2025-10-16 15:28:32 -0400

    Update EndLose.cs

--- MassArt2025_StoryB/Assets/Scripts/EndLose.cs
+++ MassArt2025_StoryB/Assets/Scripts/EndLose.cs
@@ -21,7 +21,7 @@ public class EndLose : MonoBehaviour
             endDesc.text = "You charge at the monster with all the force you possess. It is not enough. The horned monster impales you.";
         }
         else if (GameHandler.endingNumber == 3){ //  RECEPTION NEUTRAL ENDING
-            endDesc.text = "You decide the hotel is weird, so it might be best to investigate from home. You never solve the case. It will forever haunt you.";
+            endDesc.text = "You decide the hotel is weird, so you leave and try to follow other leads from the comfort of your office. You never solve the case. It will forever haunt you.";
         }
         else if (GameHandler.endingNumber == 4){ // RECEPTION BAD ENDING
             endDesc.text = "";
@@ -30,13 +30,13 @@ public class EndLose : MonoBehaviour
             endDesc.text = "Buzzboy takes a liking to you. He flirts back. You both leave to go on a nice date at the park. You have a buzzboyfriend now. Congrats.";
         }
         else if (GameHandler.endingNumber == 6){ // WORKERS BAD ENDING
-            endDesc.text = "The workers don't like rude, meddling customers. They take turns in killing you.";
+            endDesc.text = "The workers don't like rude, meddling customers. They take turns hurting you, before getting bored and killing you.";
         }
         else if (GameHandler.endingNumber == 7){ // BATHROOM BAD ENDING
-            endDesc.text = "";
+            endDesc.text = "The monster grabs you, and clamps down on you. A foul-smelling acid covers your head, and you're dissolved from the outside in. You scream, but nobody can hear you.";
         }
         else if (GameHandler.endingNumber == 8){ // CATASTROPHIC ENDING
-            endDesc.text = "The voices prompt you to open the boiler and go inside. You follow. You're now part of the hotel hivemind, and it is now strong enough to take over the world.";
+            endDesc.text = "The voices prompt you to open the boiler and go inside. You follow, and it feels just like home. Your skin melts first, becoming part of something much greater than you could ever have been. You are filled with purpose. Why did you wait so long to do this?";
         }
       
     }
