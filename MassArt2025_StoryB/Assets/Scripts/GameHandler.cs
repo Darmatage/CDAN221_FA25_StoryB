94669f60c1a77b5f8212c4eff8174aec043ef603
Darmatage <JasonWiserArt@gmail.com>
2025-10-27 21:33:09 -0400

    Added secret code to main menu elevator buttons with rennie

--- MassArt2025_StoryB/Assets/Scripts/GameHandler.cs
+++ MassArt2025_StoryB/Assets/Scripts/GameHandler.cs
@@ -5,89 +5,135 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class GameHandler : MonoBehaviour{
+public class GameHandler : MonoBehaviour {
 
-        public static string playerName = "YOUU";
-        //public static int playerStat1;
-        public static int basementWeapon;
-        /*
-                1 = rusty pipe
-                2 = toolbox
-                3 = fire axe
-        */
-        public static bool hair;
-        public static bool teeth;
-        public static bool note;
-        public static bool journal;
-        public static bool key;
-        public static bool monsterAlive;
-        public static bool gotGun;
-        public static bool singleDouble;
-        /*
-         *      true = single
-         *      false = double
-         */
-        public static int endingNumber;
-        /*
-                1 = JOKE ENDING (checkout)
-                2x =  BAD ENDING (basement)
-                        1. charge with pipe
-                        2. throw pipe
-                        3. toolbox ambush
-                        4. throw fire axe
-                3 = NEUTRAL ENDING (cold case)
-                4 = BAD ENDING (reception)
-                5 = ROMANCE ENDING
-                6 = BAD ENDING (laundry room)
-                7 = BAD ENDING (bathroom)
-                8 = CATASTROPHIC ENDING
-                9 = GOOD ENDING
-        */
-        public static string prevScene;
-        // public GameObject textGameObject;
+	public static string playerName = "YOUU";
+	//public static int playerStat1;
+	public static int basementWeapon;
+	/*
+			1 = rusty pipe
+			2 = toolbox
+			3 = fire axe
+	*/
+	public static bool hair;
+	public static bool teeth;
+	public static bool note;
+	public static bool journal;
+	public static bool key;
+	public static bool monsterAlive;
+	public static bool gotGun;
+	public static bool singleDouble;
+	/*
+	 *      true = single
+	 *      false = double
+	 */
+	public static int endingNumber;
+	/*
+			1 = JOKE ENDING (checkout)
+			2x =  BAD ENDING (basement)
+					1. charge with pipe
+					2. throw pipe
+					3. toolbox ambush
+					4. throw fire axe
+			3 = NEUTRAL ENDING (cold case)
+			4 = BAD ENDING (reception)
+			5 = ROMANCE ENDING
+			6 = BAD ENDING (laundry room)
+			7 = BAD ENDING (bathroom)
+			8 = CATASTROPHIC ENDING
+			9 = GOOD ENDING
+	*/
 
-        // void Start () { UpdateScore (); }
+	//variables for the secret elevator date ending
+	public int[] theCode = { 3, 7, 9, 11 };
+	public int[] currentCode = { 0, 0, 0, 0 };
+	public int currentCodeCount = 0;
 
-        void Update(){
-        //NOTE: delete this quit functionality when a Pause Menu is added!
-                // if (Input.GetKey("escape")){
-                //         Application.Quit();
-                // }
+	public static string prevScene;
+	// public GameObject textGameObject;
 
-                // Stat tester:
-                //if (Input.GetKey("p")){
-                //       Debug.Log("Player Stat = " + playerStat1);
-                //}
+	// void Start () { UpdateScore (); }
+
+	void Update() {
+		//NOTE: delete this quit functionality when a Pause Menu is added!
+		// if (Input.GetKey("escape")){
+		//         Application.Quit();
+		// }
+
+		// Stat tester:
+		//if (Input.GetKey("p")){
+		//       Debug.Log("Player Stat = " + playerStat1);
+		//}
+	}
+
+	// void UpdateScore () {
+	//        Text scoreTemp = textGameObject.GetComponent<Text>();
+	//        scoreTemp.text = "Score: " + score; }
+
+	public void UpdateName(string theName) {
+		playerName = theName;
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene("Scene1");
+	}
+
+	public void OpenCredits() {
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void RestartGame() {
+		gotGun = false;
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void QuitGame() {
+#if UNITY_EDITOR
+	UnityEditor.EditorApplication.isPlaying = false;
+#else
+	Application.Quit();
+#endif
+	}
+
+
+	//function for the secret elevator date ending
+	public void ElevatorCodeAttempt(int floorNum)
+	{
+		Debug.Log("you entered: " + floorNum);
+		currentCode[currentCodeCount] = floorNum;
+		currentCodeCount++;
+		if (currentCodeCount == theCode.Length)
+		{
+			//compare currentCode and theCode
+			if (CheckCodesEqual(currentCode, theCode))
+			{
+				Debug.Log("you entered the code correctly!");
+				SceneManager.LoadScene("EndWin_Date");
+			}
+			else
+			{
+				Debug.Log("That code is incorrect. Try again. thecode: " + string.Join(" ", theCode) + " currentCode: " + string.Join(" ", currentCode));
+				for (int i = 0; i < theCode.Length; i++)
+				{
+					currentCode[i] = 0;
+				}
+			}
+			currentCodeCount = 0;
+		}
+	}
+
+	public bool CheckCodesEqual(int[] array1, int[] array2)
+	{
+		for (int i = 0; i < array1.Length; i++)
+        {
+            if (!array1[i].Equals(array2[i])) // Use Equals for element comparison
+            {
+                return false;
+            }
         }
+        return true;
+	}
 
-        // void UpdateScore () {
-        //        Text scoreTemp = textGameObject.GetComponent<Text>();
-        //        scoreTemp.text = "Score: " + score; }
 
-        public void UpdateName(string theName){
-                playerName = theName;
-        }
-
-        public void StartGame(){
-                SceneManager.LoadScene("Scene1");
-        }
-
-        public void OpenCredits(){
-                SceneManager.LoadScene("Credits");
-        }
-
-        public void RestartGame(){
-                gotGun = false;
-                Time.timeScale = 1f;
-                SceneManager.LoadScene("MainMenu");
-        }
-
-        public void QuitGame(){
-                #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-                #else
-                Application.Quit();
-                #endif
-        }
-  
 }
--- MassArt2025_StoryB/Assets/Scripts/SecretHandler.cs
+++ MassArt2025_StoryB/Assets/Scripts/SecretHandler.cs
@@ -2,23 +2,25 @@ using UnityEngine;
 
 public class SecretHandler : MonoBehaviour
 {
-    public GameObject button1;
+	public int floorNum;
+/*
+	public GameObject button1;
     public GameObject button3;
     public GameObject button5;
     public GameObject button7;
     public GameObject button9;
     public GameObject button11;
     public GameObject button13;
+*/
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void ClickElevatorButton()
+	{
+		GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().ElevatorCodeAttempt(floorNum);
     }
 }
