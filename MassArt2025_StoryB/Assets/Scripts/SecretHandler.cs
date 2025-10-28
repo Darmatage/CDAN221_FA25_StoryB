87821cfffd5de57a021ada87f31af43d5cc28690
ABrickToTheHead <87443023+ABrickToTheHead@users.noreply.github.com>
2025-10-27 20:15:35 -0400

    prep for password

---
+++ MassArt2025_StoryB/Assets/Scripts/SecretHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SecretHandler : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button3;
+    public GameObject button5;
+    public GameObject button7;
+    public GameObject button9;
+    public GameObject button11;
+    public GameObject button13;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
