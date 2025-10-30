00989e1e7848550c99fa1394a60c48f2dafca95e
zadehussein05 <zmhussein2079@massart.edu>
2025-10-30 12:38:18 -0400

    ruben assets added

--- MassArt2025_StoryB/Assets/Scripts/Scene2bDialogue.cs
+++ MassArt2025_StoryB/Assets/Scripts/Scene2bDialogue.cs
@@ -129,6 +129,8 @@ public void Next(){
         }
          else if (primeInt == 22){
                 //gameHandler.AddPlayerStat(1);
+                ArtChar2a.SetActive(true);
+                ArtChar2b.SetActive(false);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Ruben";
@@ -142,6 +144,8 @@ public void Next(){
                 Char2speech.text = "";
         }
         else if (primeInt == 24){
+                ArtChar2a.SetActive(false);
+                ArtChar2b.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Ruben";
@@ -163,6 +167,8 @@ public void Next(){
         }
         else if (primeInt == 27)
         {
+         ArtChar2a.SetActive(true);
+         ArtChar2b.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "Ruben";
@@ -209,6 +215,8 @@ public void Next(){
        }
         else if (primeInt == 33){
                 //gameHandler.AddPlayerStat(1);
+                 ArtChar2a.SetActive(false);
+                ArtChar2b.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Ruben";
@@ -222,6 +230,8 @@ public void Next(){
                 Char2speech.text = "";
         }
         else if (primeInt == 35){
+                 ArtChar2c.SetActive(true);
+                ArtChar2b.SetActive(false);
                 //gameHandler.AddPlayerStat(1);
                 Char1name.text = "";
                 Char1speech.text = "";
@@ -257,6 +267,8 @@ public void Next(){
                 Char2speech.text = "";
         }
         else if (primeInt == 40){
+                ArtChar2c.SetActive(false);
+                ArtChar2a.SetActive(true);
                 //gameHandler.AddPlayerStat(1);
                 Char1name.text = "";
                 Char1speech.text = "";
@@ -264,6 +276,8 @@ public void Next(){
                 Char2speech.text = "Seems familiar...";
         }
         else if (primeInt == 41){
+                ArtChar2c.SetActive(true);
+                ArtChar2a.SetActive(false);
                 //gameHandler.AddPlayerStat(1);
                 Char1name.text = "";
                 Char1speech.text = "";
@@ -271,6 +285,8 @@ public void Next(){
                 Char2speech.text = "Got it! I think I saw her once or twice at the rest corner around the hall.";
         }
          else if (primeInt == 42){
+                ArtChar2c.SetActive(false);
+                ArtChar2b.SetActive(true);
                 //gameHandler.AddPlayerStat(1);
                 Char1name.text = "";
                 Char1speech.text = "";
@@ -286,6 +302,8 @@ public void Next(){
         }
         else if (primeInt == 44){
                 //gameHandler.AddPlayerStat(1);
+                ArtChar2a.SetActive(true);
+                ArtChar2b.SetActive(false);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Ruben";
